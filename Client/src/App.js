444425e518111c4efce138e3fc506e728f762146
christian aman <christian.aman@appliedtechnology.se>
2019-10-31 08:45:11 +0100

    Hackday start

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,15 +5,44 @@ import Browse from './components/browse/Browse';
 import AddTip from './components/addTip/AddTip';
 import Home from './components/Home';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
+import colors from '@material-ui/core/colors';
 // import Typography from '@material-ui/core/Typography';
 // import Container from '@material-ui/core/Container';
 import 'typeface-roboto';
 import './App.css';
 
-function App() {
+// const theme = createMuiTheme({
+//   typography: {
+//     button: {
+//       fontSize: '1rem',
+//       color: '#ffff00',
+//     },
+//   },
+//   button: {
+//     color: '#ffff00',
+//   },
+// });
+const GlobalCss = withStyles({
+  // @global is handled by jss-plugin-global.
+  '@global': {
+    // You should target [class*="MuiButton-root"] instead if you nest themes.
+    '.MuiButton-root': {
+      color: '#ffff00',
+    },
+    '.MuiExpansionPanelDetails-root': {
+      margin: '10rem',
+      background: '#fff00',
+    },
+  },
+})(() => null);
+
+const App = () => {
   return (
     <div className='App'>
       <CssBaseline />
+      <GlobalCss />
+      {/* <ThemeProvider theme={theme}> */}
       <header className='App-header'>
         <Router>
           <Header />
@@ -24,8 +53,9 @@ function App() {
           </Switch>
         </Router>
       </header>
+      {/* </ThemeProvider> */}
     </div>
   );
-}
+};
 
 export default App;
--- Client/src/components/browse/Card.js
+++ Client/src/components/browse/Card.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
 // import PropTypes from 'prop-types';
 
 const Card = props => {
-  const { title, desc, link } = props;
+  const { id, title, desc, link } = props;
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{desc}</p>
-      <span>
-        <a href={link}>{link}</a>
-      </span>
+    <div className={classes.root}>
+      <ExpansionPanel square={true} spacing={true}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1a-content'
+          id='panel1a-header'>
+          <Typography className={classes.heading}>{title}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails expanded={true}>
+          <Typography>
+            {desc}
+            <Link href={link}>Read more...</Link>
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
   );
 };
